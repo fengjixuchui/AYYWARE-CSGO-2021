97727fa9e61b08078fe804c198a90a3b9680463b
helloobaby <sun888486@qq.com>
2021-06-17 12:14:07 +0800

    update

--- Hooks.cpp
+++ Hooks.cpp
@@ -75,17 +75,14 @@ void Hooks::Initialise()
 	// Panel hooks for drawing to the screen via surface functions
 	VMTPanel.Initialise((DWORD*)Interfaces::Panels);
 	oPaintTraverse = (PaintTraverse_)VMTPanel.HookMethod((DWORD)&PaintTraverse_Hooked, Offsets::VMT::Panel_PaintTraverse);
-	//Utilities::Log("Paint Traverse Hooked");
 
 	// No Visual Recoi	l
 	VMTPrediction.Initialise((DWORD*)Interfaces::Prediction);
 	VMTPrediction.HookMethod((DWORD)&Hooked_InPrediction, 14);
-	//Utilities::Log("InPrediction Hooked");
 
 	// Chams
 	VMTModelRender.Initialise((DWORD*)Interfaces::ModelRender);
 	oDrawModelExecute = (DrawModelEx_)VMTModelRender.HookMethod((DWORD)&Hooked_DrawModelExecute, Offsets::VMT::ModelRender_DrawModelExecute);
-	//Utilities::Log("DrawModelExecute Hooked");
 
 	// Setup ClientMode Hooks
 	VMTClientMode.Initialise((DWORD*)Interfaces::ClientMode);
@@ -179,11 +176,6 @@ void ClanTag()
 
 bool __stdcall CreateMoveClient_Hooked(/*void* self, int edx,*/ float frametime, CUserCmd* pCmd)
 {
-	static bool log_once = 0;
-	if (!log_once)
-		Utilities::Log("%s", __FUNCTION__);
-	log_once = 1;
-
 
 	if (!pCmd->command_number)
 		return true;
--- README.md
+++ README.md
@@ -1,24 +1,8 @@
-# Ayyware-2021
-
-This project is based on Ayyware that for learn
-
-All functions have tested on official server.
-
 ![image](https://github.com/helloobaby/alwayslose/blob/master/spinbot.gif)
 
 
-How to use:
-	
-ReflectiveInject this Dll
-
-if you don't have injector
-
-see https://github.com/helloobaby/AYYWARE-injector
-
 Tips:
 	
-If you compile this project, do not open the inline function extension, MSVC will over-
-
-optimize the code, this will crash the game.
+If you want to compile this project, make sure do not open the inline function extension due to the over-potimize.
 
 
